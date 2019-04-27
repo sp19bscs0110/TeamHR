8dfa80a9972fb0ca1388dadfb6fda8fd42b7edc5
Hammad Rasheed <49777507+sp19bscs0110@users.noreply.github.com>
2019-04-27 23:43:12 +0500

    Update project_py.py

--- project_py.py
+++ project_py.py
@@ -10,3 +10,26 @@ Original file is located at
 print("Name:Shahmeer ")
 print("Section:BM")
 print("ID:sp19-bsse-0007")
+#codeimagetransfer
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+#Shahmeer
+#Sp19bsse0007
+
+import socket
+s= socket.socket()
+host=input(str("Please ener the host address of the sender :"))
+port = 8080
+s.connect((host,port))
+print("connected...")
+filename = input(str("Please enter a filename for the incoming file: "))
+file = open(filename,'wb')
+file_data = s.recv(1024)
+file.write(file_data)
+file.close()
+print("Fle has been recieve succesfully.")
