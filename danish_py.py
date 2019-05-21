1ca9e5628fcdd85b4a31370df89c53a8aa52b932
sp19-bsse-0006 <49778164+sp19-bsse-0006@users.noreply.github.com>
2019-05-21 17:37:34 +0500

    Update danish_py.py

--- danish_py.py
+++ danish_py.py
@@ -1,3 +1,25 @@
+#image capture code
+from PIL import ImageGrab
+import os
+import time
+
+def screenGrab():
+    box = ()
+
+# this line creates a variable to hold the screenshot
+    im = ImageGrab.grab()
+
+# This command will save the screenshot into a time-stamped png file
+# FYI - getcwd() is get Current Working Directory
+    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) + '.png', 'PNG')
+    im.show()
+    #im.write()
+screenGrab()
+
+
+
+
+
 # -*- coding: utf-8 -*-
 """danish.py
 
